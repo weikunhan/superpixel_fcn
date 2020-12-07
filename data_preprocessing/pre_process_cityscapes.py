5a062dc9e24c768eba15e9ee5fd3df23b33e32d4
Weikun Han <weikunhan@g.ucla.edu>
2020-12-06 20:11:02 -0800

    add pre_process_cityscapes

--- data_preprocessing/pre_process_cityscapes.py
+++ data_preprocessing/pre_process_cityscapes.py
@@ -6,73 +6,140 @@ Reference:
 - https://github.com/fuy34/superpixel_fcn/blob/master/data_preprocessing/pre_process_bsd500.py
 """
 
+import cv2
 import os
 import glob
+import shutil
 import argparse
+from PIL import Image
+from multiprocessing import Pool
+from tqdm import tqdm
 
-
-def make_dataset(dir):
-    cwd = os.getcwd()
-    train_list_path = cwd + '/train.txt'
-    val_list_path =  cwd + '/val.txt'
+def make_dataset(data_dir, image_flag=True):
     train_list = []
     val_list = []
+    train_folder_list = glob.glob(os.path.join(data_dir, '*'))
+    val_folder_list = glob.glob(os.path.join(data_dir, '*'))
+    
+    if image_flag: 
+        for folder_path in train_folder_list:
+            for file_path in glob.glob(os.path.join(folder_path, '*.png'))
+                train_list.append(file_path)
+    else:
+        for folder_path in train_folder_list:
+            for file_path in glob.glob(os.path.join(folder_path, '*xxxx_.png'))
+                train_list.append(file_path)       
+            
+    if image_flag:
+        for folder_path in val_folder_list:
+            for file_path in glob.glob(os.path.join(folder_path, '*.png'))
+                val_list.append(file_path)
+    else:
+        for folder_path in val_folder_list:
+            for file_path in glob.glob(os.path.join(folder_path, '*xxxx_.png'))
+                val_list.append(file_path)        
 
-    try:
-        with open(train_list_path, 'r') as tf:
-            train_list_0 = tf.readlines()
-            for path in train_list_0:
-                img_path = os.path.join(dir, 'BSR/BSDS500/data/images/train', path[:-1]+ '.jpg' )
-                if not os.path.isfile(img_path):
-                    print('The validate images are missing in {}'.format(os.path.dirname(img_path)))
-                    print('Please pre-process the BSDS500 as README states and provide the correct dataset path.')
-                    exit(1)
-                train_list.append(img_path)
+    if len(train_list) != 2975:
+        raise AssertionError('The training dataset is supported to have 2975 samples!')
 
-        with open (val_list_path, 'r') as vf:
-            val_list_0 = vf.readlines()
-            for path in val_list_0:
-                img_path = os.path.join(dir, 'BSR/BSDS500/data/images/val', path[:-1]+ '.jpg')
-                if not os.path.isfile(img_path):
-                    print('The validate images are missing in {}'.format(os.path.dirname(img_path)))
-                    print('Please pre-process the BSDS500 as README states and provide the correct dataset path.')
-                    exit(1)
-                val_list.append(img_path)
-
-
-    except IOError:
-        print ('Error No avaliable list ')
-        return
-
+    if len(val_list) != 500:
+        raise AssertionError('The training dataset is supported to have 500 samples!')
+    
     return train_list, val_list
 
+def save_images(image_patch_in, image_path_out):
+    if os.path.isfile(image_path_out):
+        try:
+            Image.open(image_path_out).verify()
+
+            return 
+        except Exception as e:
+            pass
+
+    image = cv2.imread(image_patch_in)
+    cv2.imwrite(image_path_out, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+def generate_image_dataset(train_data_dir_out, val_data_dir_out,
+                           image_train_list, image_val_list):
+    pool = Pool() 
+
+    for file_name in tqdm(image_train_list, total= len(image_train_list), 
+                           desc='Start train image proccessing'):
+        folder_name = file_name.split(os.path.sep)[-2]
+        temp_folder = os.path.join(train_data_dir_out, folder_name)
+        file_name = file_name.split(os.path.sep)[-1].split('.')[0] + '_img.jpg'
+        temp_file_name = os.path.join(temp_folder, file_name)
+
+        if not os.path.exists(temp_folder):
+            os.mkdir(temp_folder)
+        
+        pool.apply_async(save_images, (file_name, temp_file_name))
+
+
+    for file_name in tqdm(image_val_list, total= len(image_val_list), 
+                           desc='Start val image proccessing'):
+        folder_name = file_name.split(os.path.sep)[-2]
+        temp_folder = os.path.join(val_data_dir_out, folder_name)
+        file_name = file_name.split(os.path.sep)[-1].split('.')[0] + '_img.jpg'
+        temp_file_name = os.path.join(temp_folder, file_name)
+
+        if not os.path.exists(temp_folder):
+            os.mkdir(temp_folder)
+        
+        pool.apply_async(save_images, (file_name, temp_file_name))
+        
+    pool.close()
+    pool.join()
+
+def generate_label_dataset(train_data_dir_out, val_data_dir_out,
+                           lable_train_list, label_val_list):
+    for file_name in tqdm(lable_train_list, total= len(lable_train_list), 
+                           desc='Start train label proccessing'):
+        folder_name = file_name.split(os.path.sep)[-2]
+        file_name = file_name.split(os.path.sep)[-1].split('.')[0] + '_label.png'
+        temp_file_name = os.path.join(train_data_dir_out, folder_name, file_name)
+        shutil.copyfile(file_name, temp_file_name)
+
+    for file_name in tqdm(label_val_list, total= len(label_val_list), 
+                           desc='Start val label proccessing'):
+        folder_name = file_name.split(os.path.sep)[-2]
+        file_name = file_name.split(os.path.sep)[-1].split('.')[0] + '_label.png'
+        temp_file_name = os.path.join(val_data_dir_out, folder_name, file_name)
+        shutil.copyfile(file_name, temp_file_name)
+    
 def main():
-    datadir = args.dataset
-    train_list, val_list = make_dataset(datadir)
-    dump_pth = os.path.abspath(args.dump_root)
-    print("data will be saved to {}".format(dump_pth))
+    data_dir_in = args.dataset
+    data_dir_out = os.path.abspath(args.dump_root)
 
-    # mutil-thread running for speed
-    Parallel(n_jobs=args.num_threads)(delayed(dump_example)(n, len(train_list),'train', train_samp) for n, train_samp in enumerate(train_list))
-    Parallel(n_jobs=args.num_threads)(delayed(dump_example)(n, len(train_list),'val', val_samp) for n, val_samp in enumerate(val_list))
+    print("Data will be saved to: {}".format(data_dir_out))
 
-    with open(dump_pth + '/train.txt', 'w') as trnf:
-        imfiles = glob(os.path.join(dump_pth, 'train', '*_img.jpg'))
-        
-        for frame in imfiles:
-            trnf.write(frame + '\n')
+    image_data_dir_in = os.path.join(data_dir_in, 'xxxx_.')
+    label_data_dir_in = os.path.join(data_dir_in, 'xxxx_.')
+    train_data_dir_out = os.path.join(data_dir_out, 'train')
+    val_data_dir_out = os.path.join(data_dir_out, 'val')
+    image_train_list, image_val_list = make_dataset(image_data_dir_in, image_flag=True)
+    lable_train_list, label_val_list = make_dataset(label_data_dir_in, image_flag=False)
+    generate_image_dataset(train_data_dir_out, val_data_dir_out,
+                           image_train_list, image_val_list)
+    generate_label_datasent(train_data_dir_out, val_data_dir_out,
+                            lable_train_list, label_val_list)
 
-    with open(dump_pth+ '/val.txt', 'w') as trnf:
-        imfiles = glob(os.path.join(dump_pth, 'val', '*_img.jpg'))
-        
-        for frame in imfiles:
-            trnf.write(frame + '\n')
+    with open(os.path.join(data_dir_out, 'train.txt'), 'w') as f:
+        for folder_path in glob.glob(os.path.join(train_data_dir_out, '*')):
+            for file_path in glob.glob(os.path.join(folder_path, '*_img.jpg'))
+                for file_name in file_path:
+                    f.write(file_name + '\n')
+
+    with open(os.path.join(data_dir_out, 'val.txt'), 'w') as f:
+        for folder_path in glob.glob(os.path.join(val_data_dir_out, '*')):
+            for file_path in glob.glob(os.path.join(folder_path, '*_img.jpg'))
+                for file_name in file_path:
+                    f.write(file_name + '\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default="", help="where the filtered dataset is stored")
     parser.add_argument("--dump_root", type=str, default="", help="Where to dump the data")
-    parser.add_argument("--num_threads", type=int, default=4, help="number of threads to use")
     args = parser.parse_args()
 
     main()

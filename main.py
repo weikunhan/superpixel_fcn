5e558f0dc908aac5e607a174305029369b4b7561
Weikun Han <weikunhan@g.ucla.edu>
2020-12-10 08:00:15 -0800

    update config

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ parser.add_argument('--data', metavar='DIR',default='', help='path to input data
 parser.add_argument('--savepath',default='', help='path to save ckpt')
 
 
-parser.add_argument('--train_img_height', '-t_imgH', default=208,  type=int, help='img height')
-parser.add_argument('--train_img_width', '-t_imgW', default=208, type=int, help='img width')
+parser.add_argument('--train_img_height', '-t_imgH', default=768,  type=int, help='img height')
+parser.add_argument('--train_img_width', '-t_imgW', default=768, type=int, help='img width')
 parser.add_argument('--input_img_height', '-v_imgH', default=320, type=int, help='img height_must be 16*n')  #
 parser.add_argument('--input_img_width', '-v_imgW', default=320,  type=int, help='img width must be 16*n')
 
